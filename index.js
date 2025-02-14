8dbeb3b598535662eb1c05e29248b21ceea22323
KISHORE0459 <111626417+KISHORE0459@users.noreply.github.com>
2025-02-14 12:07:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
+const express = require("express"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+}); //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
